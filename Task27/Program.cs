832f9f6466d762edc1b3ea1d0718ca911656accc
unknown <ekaterina.ordynets@yandex.by>
2022-10-12 21:44:52 +0300

    Add Task27

---
+++ Task27/Program.cs
@@ -0,0 +1,20 @@
+﻿//  Напишите программу, которая принимает на вход число
+// и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int newV = 0;
+int sum = 0;
+while (number > 0)
+{
+    int dig = number % 10;//% берёт остаток от деления
+    sum += dig;
+    newV = newV * 10 + dig;
+    number = number / 10;
+}
+Console.WriteLine("Сумма чисел " + sum);
+Console.ReadKey();
